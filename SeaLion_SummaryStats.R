13273829b9f72a49bf74d92acdb674643dbb0805
kellykapsar <kelly.kapsar@gmail.com>
2021-04-14 17:21:16 -0500

    Create files for telemetry data summary statistics and also trajectory modeling/dynamic interactions of ships and SSLs.

---
+++ SeaLion_SummaryStats.R
@@ -0,0 +1,6 @@
+# Tag duration values from Progress Report: 
+# https://drive.google.com/drive/u/0/folders/1O4Pmj-SGIdaV3tSbhV7BIahFfnsSEWLi
+
+tags <- c(235, 161, 209, 248, 158,184, 173, 164, 205, 202, 202, 262)  
+mean(tags)
+sd(tags)
