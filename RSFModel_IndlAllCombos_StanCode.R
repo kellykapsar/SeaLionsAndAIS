40dbf18760621f77947a0f99081d8dbaea2c400f
kellykapsar <kelly.kapsar@gmail.com>
2021-11-04 18:44:01 -0500

    Attempt at customization. Going to switch it back to code from step2dev.

--- RSFModel_IndlAllCombos_StanCode.R
+++ RSFModel_IndlAllCombos_StanCode.R
@@ -33,57 +33,68 @@ modpath <- paste(resultdir, "model.stan", sep = "")
 # Write the stan model
 write(
   "data{
-    int C;  // the number of choices per set
-    int K;  // the number of slope parameters per individual
-    int N;  // the number of observations
-    matrix[C, K] x[N];  // variables in design matrix format
-    int y[N]; // index of which alternative was selected
-    vector[C] obs; //variable used in test-stat calculation
-    vector[C] pos[C];
+  int C;  // the number of choices per set
+  int K;  // the number of slope parameters per individual
+  int N;  // the number of observations
+  int nWks;  // the number of weeks
+  int wk[N];  // indexes the week associated with each observation
+  matrix[C, K] x[N];  // variables in design matrix format
+  int y[N]; // index of which alternative was selected
+  vector[C] obs;
+  vector[C] pos[C];
+
   }
   
   parameters{
-    vector[K] beta;  // rsf coefficients
+  vector[K] beta[nWks];  // slope parameters for each week
+  vector[K] mu;  // mean for each slope parameter
+  vector<lower = 0>[K] stdev;  // standard deviation of each slope parameter
   }
   
   model{
-    
-    for(l in 1:K){
-      beta[l] ~ normal(0, 10);  // prior distribution for slope parameter mean
-    }
+  // vector[C] psi[N];
   
-    for(i in 1:N){
-      y[i] ~ categorical_logit(softmax(x[i] * beta));
-    }
+  //for(l in 1:K){           // Attempting to vectorize, removing loop
+  mu ~ normal(0, 1);  // prior distribution for slope parameter mean
+  stdev ~ normal(0, 1); // prior distribution for slope parameter sd
+  //}
+  
+  for(j in 1:nWks){
+  //for (k in 1:K){    
+  beta[j] ~ normal(mu, stdev);
+  }
+  // } 
+  
+  for(i in 1:N){
+  y ~ categorical_logit(x[i] * beta[wk[i]]);
+  }
   }
   
   generated quantities{
-    simplex[C] expected[N]; //the probabilities of use from dc_model
-    vector[N] chis_obs_i;   //chi-square value for each choice set using observed data
-    vector[N] chis_sim_i;  // chi-square value from simulated data
-    real chis_obs;  //sum of chi square values across all choice sets
-    real chis_sim;  //sum of chi square values across all choice sets
-    vector[C] rch[N];  //simulated random choice of used alt. within obs. data sets
-    vector[N] log_lik; // log likelihood
-    int rcat[N];
+  simplex[C] expected[N]; //probabilities of use from dc model
+  vector[N] chis_obs_i; //chi-square value for each choice based on observations
+  vector[N] chis_sim_i; // chi-square value from simulated data
+  real chis_obs; //sum of chi-square values
+  real chis_sim;
+  vector[C] rch[N]; //simulated random choice of used alternative
+  int rcat[N];
+
   
-    for(a in 1:N){
-      expected[a] = softmax(x[a] * beta);
-  
-      rcat[a] = categorical_rng(expected[a]);
-      rch[a] = pos[rcat[a]];
-  
-      chis_obs_i[a] = sum(((obs - expected[a]) .* (obs - expected[a])) ./
-                         expected[a]);
-      chis_sim_i[a] = sum(((rch[a] - expected[a]) .* (rch[a] - expected[a])) ./
-                         expected[a]);
-    
-      log_lik[a] = categorical_logit_lpmf(y[a]| x[a] * beta);
-    }
-    chis_obs = sum(chis_obs_i);
-    chis_sim = sum(chis_sim_i);
+  for(a in 1:N){
+    expected[a] = softmax(x[a] * beta[wk[a]]);
+
+    rcat[a] = categorical_rng(expected[a]);
+    rch[a] = pos[rcat[a]]; 
+    chis_obs_i[a] = sum(((obs - expected[a]) .* (obs - expected[a])) ./
+                        expected[a]);
+    chis_sim_i[a] = sum(((rch[a] - expected[a]) .* (rch[a] - expected[a])) ./
+                        expected[a]);
+  }
+  chis_obs = sum(chis_obs_i);
+  chis_sim = sum(chis_sim_i);
   
   }
+
 "
 , file = modpath)
 
