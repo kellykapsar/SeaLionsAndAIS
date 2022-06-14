f96a78f9f8d1b6ab3f09b55686b708813a0ff63f
kellykapsar <kelly.kapsar@gmail.com>
2022-06-14 10:05:57 -0500

    Modify for seasonal vessel data extraction.

--- S2b_DistanceToVessel_HPCC_WithinHR.R
+++ S2b_DistanceToVessel_HPCC_WithinHR.R
@@ -1,5 +1,6 @@
 
 
+# Load libraries
 library(tidyr)
 library(dplyr)
 library(sf)
@@ -9,22 +10,25 @@ library(fasterize)
 library(foreach)
 library(doParallel)
 
-# weekly homerange polygons
-hr <- st_read("../Data_Processed/Telemetry/Homerange_KDE_season_20220610.shp")
+# Load functions
+source("./S0_Functions.R")
 
-hr$deploy_id <- substr(hr$wklyhr_, 1, 13)
-hr$year <- substr(hr$wklyhr_, 14,17)
-hr$week <- substr(hr$wklyhr_, 18,19)
+# weekly homerange polygons
+hr <- st_read("../Data_Processed/Telemetry/Homerange_KDE_season_20220614.shp")
+
+hr$deploy_id <- substr(hr$ssnhr_d, 1, 13)
+hr$year <- substr(hr$ssnhr_d, nchar(hr$ssnhr_d)-3, nchar(hr$ssnhr_d))
+hr$season <- substr(hr$ssnhr_d, 15,nchar(hr$ssnhr_d))
 
 # Sea lion data from 2_SSLAvailAndCovarExtraction.Rmd
 # ssl5 <- readRDS(file="../Data/ssl5.rds")
 load(file="../Data_Processed/Telemetry/TEMP_seasonal.rda")
 
 # Vessel tracklines
-ships <- readRDS( "../Data_Processed/AIS/Becto/AIS/AllVessels_Reprojected.rds")
+ships <- readRDS( "../Data_Raw/AIS_SSLWeeklySubset/Vector/EPSG32605/AllVessels_Reprojected.rds")
 
 # Land raster
-land <- raster("../Data/Bathymetry.tif")
+land <- raster("../Data_Processed/Bathymetry.tif")
 land[values(land) > 0] <- 1
 land[values(land) < 0] <- NA
 
@@ -42,10 +46,10 @@ registerDoParallel(cores=as.numeric(Sys.getenv("SLURM_CPUS_ON_NODE")[1]))
 ############# CALCULATE PROXIMITY TO VESSELS ############# 
 ##################################################################
 # Standardize dates 
-ships$date <- as.Date(substr(ships$AIS_ID, 11, 18),format = "%Y%m%d") %>% format(., "%G-W%V")
+ships$season <- as.Date(substr(ships$AIS_ID, 11, 18),format = "%Y%m%d") %>% getSeasonYear()
 
 # ID unique SSL/week combos 
-weeklyhr_ids <- unique(ssl4$weeklyhr_id)
+seasonhr_idshr_ids <- unique(ssl4$seasonhr_id)
 
 # Separate fishing vessel from other vesels 
 nofish <- ships[ships$AIS_Typ != "Fishing",]
@@ -56,12 +60,10 @@ fish <- ships[ships$AIS_Typ == "Fishing",]
 # of land pixels 
 extractcostdist <- function(pts, lines, land, hrs){
   # Id correct homerange polygon 
-  polyhr <- hrs[which(hrs$deploy_id == pts$deploy_id[1] & 
-                        hrs$year == pts$year[1] &
-                        hrs$week == pts$weekofyear[1]),]
+  polyhr <- hrs[which(hrs$ssnhr_d == pts$seasonhr_id[1]),]
   polyhr <- st_buffer(polyhr, 100000)
-  # Id lines within the date range
-  lines <- lines[lines$date == pts$date[1],]
+  # Id lines within the correct season
+  lines <- lines[lines$season == pts$season[1],]
   # Isolate lines that spatially intersect homerange
   linesin <- lines[st_intersects(polyhr, lines, sparse=FALSE),]
   if(length(linesin$AIS_ID) == 0){return(NA)}
@@ -91,20 +93,20 @@ extractcostdist <- function(pts, lines, land, hrs){
 # Extract pixel values from cost distance raster
 start <- proc.time()
 
-ssl5 <- foreach(i = 1:length(weeklyhr_ids), .packages = c("raster", "sf", "dplyr", "tidyr", "stars")) %dopar%{
+ssl5 <- foreach(i = 1:length(seasonhr_ids), .packages = c("raster", "sf", "dplyr", "tidyr", "stars")) %dopar%{
   print(i)
-  pts <- ssl4[which(ssl4$weeklyhr_id == weeklyhr_ids[i]),]
+  pts <- ssl4[which(ssl4$seasonhr_id == seasonhr_ids[i]),]
   
   # FISH
   costdistfish <- extractcostdist(pts, fish, land, hr)
   pts$prox_fish_km_new <- NA
   
   if(class(costdistfish) == "logical"){
-    print(paste0(weeklyhr_ids[i], " removed."))
+    print(paste0(seasonhr_ids[i], " removed."))
   }
   valsfish <- st_intersects(pts, costdistfish)
   
-  tempfish  <- lapply(1:length(pts$date_outer), function(x){mean(costdistfish$layer[valsfish[[x]]])})
+  tempfish  <- lapply(1:length(pts$easting), function(x){mean(costdistfish$layer[valsfish[[x]]])})
   pts$prox_fish_km_new <- unlist(tempfish)
   
   # SHIPS 
@@ -116,8 +118,8 @@ ssl5 <- foreach(i = 1:length(weeklyhr_ids), .packages = c("raster", "sf", "dplyr
   }
   vals <- st_intersects(pts, costdist)
   
-  temp  <- lapply(1:length(pts$date_outer), function(x){mean(costdist$layer[vals[[x]]])})
+  temp  <- lapply(1:length(pts$easting), function(x){mean(costdist$layer[vals[[x]]])})
   pts$prox_ship_km_new <- unlist(temp)
   
-  saveRDS(pts, paste0("../Data/ssl5/",weeklyhr_ids[i],".rds"))
+  saveRDS(pts, paste0("../Data/ssl5/",seasonhr_ids[i],".rds"))
 }
