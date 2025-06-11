948f29865c1495acb8e79e4847ec6ffb525ffc43
walovens <walovens@msu.edu>
2025-06-11 10:50:38 -0400

    state assignments and probabilities

--- 3_FitHiddenMarkovModels_SSL_SW.R
+++ 3_FitHiddenMarkovModels_SSL_SW.R
@@ -121,8 +121,8 @@ mu0 <- c(0.1, 1)
 sigma0 <- c(0.1, 1)
 
 # Zero-distribution term. 
-# zeromass0 <- c(0.1, 0.05) If there were only one zero. 
-zeromass0 <- c(0.15, 0.1) # Since there are more frequent zeros, we'll increase.  
+zeromass0 <- c(0.1, 0.05) # Only one zero.
+# zeromass0 <- c(0.15, 0.1) # Since there are more frequent zeros, we'll increase.
 
 # Assigning the step distribution starting values
 stepPar0 <- c(mu0,
@@ -175,3 +175,105 @@ ssl_m_null
 # State 1 has a smaller mean step length than state 2 around 105m (may vary with each run), with mean for state 2 around 491. The zero-mass values are both very small since there were not that many zero step length values.
 
 # The concentration represents a sort of variance around the mean turning angles. Both states have a mean turning angle close to 0. A relative turn angle of zero means continued movement in the same direction.
+
+# There is only an intercept regression coefficient since we did not test a regression equation.
+
+# The transition matrix shows the average probability of transitioning from state 1 to state 2 is 10.8% and transitioning from state 2 to state 1 is also 10.8%. This indicates that once an animal is in a state it tends to stay there and transitions between states are relatively rare.
+
+# View plots. State 1 is orange and state 2 is blue. Path segments are colored according to predicted state in all the full trajectory plots of each animal. Longer distance straight movements are colored as state 2 (blue) and more resident/locally focused movements as state 1 (orange). It is likely here that state 1 is more of a local foraging state while state 2 is directed travel. 
+plot(ssl_m_null,
+     ask = F)
+# hit the back button in the plotting window to see all plots
+
+
+# State Assignments and Probabilities -------------------------------------
+
+# To globally decode the state process, the Viterbi algorithm is implemented in the function viterbi(). This function outputs the most likely sequence of states to have generated the observations, under the fitted model. 
+
+ssl_states_2 <- viterbi(ssl_m_null)
+
+# Look at the most probable states for the first 25 observations of the first individual.
+ssl_states_2[1:25]
+# The first 6 are state 1 and the rest are state 2.
+
+# ssl_states_2 gives us a predicted state for each location in the dataset. This allows us to look at the proportion of time spent in each state.
+prop.table(table(ssl_states_2))
+
+# Overall, these sea lions spent about half their time in a resident state and half their time in a more nomadic or migratory state.
+
+stateProps <- ssl_data %>% 
+  
+  # Add predicted state information to initial data frame
+  mutate(state = ssl_states_2) %>% 
+  
+  # Add new column that is the total locations for each animal
+  mutate(locs = n(),
+         .by = ID) %>% 
+  
+  # Summarize for each animal, and each state, the proportion of locations
+  summarize(stateProp = n()/locs,
+            .by = c(ID, state)) %>% 
+  
+  # Unique rows of information only
+  distinct()
+
+# You can compare the proportions of each states visually to the maps to see how it matches up.
+
+# Plot this across individuals 
+stateProps %>% 
+  filter(state == 1) %>% 
+  mutate(ID = fct_reorder(ID, # Order the ID column by the value of stateProp.
+                          stateProp)) %>% 
+  
+  ggplot(aes(y = stateProp,
+             x = ID,
+             fill = ID)) +
+  geom_col(col = "black",
+           position = position_dodge()) +
+  coord_flip() +
+  labs(y = "Prop. time in State 1 (foraging/encamped)") +
+  theme_bw()
+
+# SSL2019785KOD spends the most time in this local resident state. Let's look at their trajectory again.
+plot(ssl_move[ssl_move$ID == "SSL2019785KOD", ],
+     ask = F)
+
+# Differentiating between individual animals in terms of overall movement strategy can be better done with a net-squared displacement model or even mean-squared displacement. HMM are used more to categorize segments as particular behavioral states.
+
+# * Could further specify this analysis by splitting different times of year or different seasons?
+
+# Look at actual probabilities assigned to each location for each behavioral state
+ssl_probs_2 <- stateProbs(ssl_m_null)
+
+# Output is a matrix. One row for each location. Values across a row should add to 1.0.
+head(ssl_probs_2)
+nrow(ssl_probs_2)
+# The state with the highest probability according to stateProbs() might not be the same as the state in the most probable sequence returned by the Viterbi algorithm. This is because the Viterbi algorithm performs "global decoding" and the state probabilities are "local decoding".
+
+# Visualize the results of viterbi() and stateProbs(). This plot shows the plots of the most likely state sequence decoded by the Viterbi algorithm, as well as both columns of the matrix of state probabilities, for one individual "SSL2019785KOD".
+plotStates(ssl_m_null,
+           animals = "SSL2019785KOD",
+           ask = F)
+
+# This information can be associated with all kinds of variables (e.g., temperature, NDVI, breeding state, etc)
+
+# Custom plot of same information
+ssl_data %>% 
+  mutate(state = as.factor(ssl_states_2)) %>% 
+  filter(ID == "SSL2019785KOD") %>% 
+  ggplot(aes(x = x_,
+             y = y_,
+             fill = state,
+             col = state)) +
+  geom_path(alpha = 0.5) +
+  geom_point(shape = 21,
+             alpha = 0.8,
+             col = "black") +
+  scale_fill_manual(values = c("orange",
+                               "cornflowerblue")) +
+  scale_color_manual(values = c("orange",
+                                "cornflowerblue")) +
+  theme_minimal() +
+  labs(x = "x",
+       y = "y",
+       title = "SSL2019785KOD")
